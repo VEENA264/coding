8b0c077d1d0146cc9891c89d4447e18c39a55d3b
VEENA264 <sugateveena@gmail.com>
2025-02-13 21:37:41 -0800

    changed input string

--- Demo.java
+++ Demo.java
@@ -5,6 +5,6 @@ public class Demo
            
             {
               
-               System.out.println("Hello java")
+               System.out.println("Hello CBKPC")
             }
       }
